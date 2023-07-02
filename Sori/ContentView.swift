fd438670bd102d71d5f62b11d8303adaa03aa894
TortasMcFly <hectorluevanov@gmail.com>
2023-07-02 12:22:46 -0600

    Initial Commit

---
+++ Sori/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sori
+//
+//  Created by Hector Luevano on 02/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sori/SoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoriApp.swift
+//  Sori
+//
+//  Created by Hector Luevano on 02/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
